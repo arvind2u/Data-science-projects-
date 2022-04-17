b197cdca0d28a774068de73d9c73292a4a0a17ac
Arvind Thorat <88592582+arvind2u@users.noreply.github.com>
2022-04-17 16:12:29 +0530

    Update saved_model.py

--- social_network Ads/saved_model.py
+++ social_network Ads/saved_model.py
@@ -2,6 +2,6 @@ import json
 import requests
 
 url  = 'http://127.0.0.1:8009/model'
-request_data = json.dumps('model','SVM')
+request_data = json.dumps('model','KNN')
 response = request.post(url, request_data)
 print(response_text)
